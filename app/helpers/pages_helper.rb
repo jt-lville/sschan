daedd6d321a972b6fbbb1cdd32be810805b06c51
Jonathan Tang <kyothine@gmail.com>
2011-12-29 13:03:21 -0500

    cute random phrases

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,4 +8,24 @@
      # "#{base_title} | #{@title}"
     #end
   end
+
+  def random_phrase
+
+    r_num = rand(10)
+    str = ""
+
+    if r_num == 0
+      str = "C'mon Simon! Use your fighting spirit!"
+    elsif r_num == 1
+      str = "I'm not going there to die. I'm going to see if I'm really alive. "
+    elsif r_num == 2
+      str = "Do you know where hell is...? It's in your mind."
+    else
+      str = "Elements of Harmony...I know I've heard that from somewhere."
+    end
+
+    return str
+
+  end
+
 end
