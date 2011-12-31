5c94a7c803c8c7e4a2d3df5f126678085af26955
Jonathan Tang <kyothine@gmail.com>
2011-12-31 15:37:31 -0500

    reverse admin roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     user ||= User.new # guest user (not logged in)
 		
-		if user.role? :admin or user.paradigm == "pretension"
+		if user.role? :admin# or user.paradigm == "pretension"
 			can :manage, :all
 		else		
 			can :manage, [Post, Comment, User]
