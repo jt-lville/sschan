0ff12b13272b09ee8f827a9a1e5e7fc0012ba120
Jonathan Tang <kyothine@gmail.com>
2012-01-01 22:03:58 -0500

    home page is viewable, images changed to sschan, not ssc

--- app/controllers/beta_pages_controller.rb
+++ app/controllers/beta_pages_controller.rb
@@ -1,7 +1,7 @@
 class BetaPagesController < ApplicationController
 	layout 'beta'
 	
-	before_filter :check_login
+	#before_filter :check_login
 	
 	def check_login
 		redirect_to '/login' if not user_signed_in?
